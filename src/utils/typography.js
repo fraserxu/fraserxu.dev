cc11d818e7d5b2d3cc41d7a95d996525bf2628b4
Fraser Xu <xvfeng123@gmail.com>
2020-04-27 19:50:56 +1000

    add svg

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,14 @@
-import Typography from "typography"
-import fairyGateTheme from "typography-theme-fairy-gates"
+import Typography from 'typography'
+import fairyGateTheme from 'typography-theme-fairy-gates'
 
-const typography = new Typography(fairyGateTheme)
+const typography = new Typography({
+  ...fairyGateTheme,
+  overrideThemeStyles: () => ({
+    a: {
+      backgroundImage: 'none',
+    },
+  }),
+})
 
 export const { scale, rhythm, options } = typography
 export default typography
