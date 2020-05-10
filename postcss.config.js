a937c56f3d6bbf66369b9d82d1fcbe6a105656fd
Fraser Xu <xvfeng123@gmail.com>
2020-05-10 23:22:31 +1000

    rm plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,3 @@
 module.exports = () => ({
-  plugins: [
-    require('tailwindcss'),
-    require('postcss-nesting'),
-    process.env.NODE_ENV === 'production' &&
-      require('@fullhuman/postcss-purgecss')({
-        content: ['./src/**/*.js'],
-        defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-        whitelistPatternsChildren: [/^markdown$/, /^token/, /^pre/, /^code/],
-      }),
-  ],
+  plugins: [require('tailwindcss'), require('postcss-nesting')],
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 // tailwind.config.js
 module.exports = {
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.js'],
+  },
   theme: {
     fontFamily: {
       sans: ['Work Sans', 'sans-serif'],
